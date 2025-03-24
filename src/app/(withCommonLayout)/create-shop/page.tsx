7d87de6a3fceac6cc43b282cc89c253e3dfb08d6
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-03-24 20:55:17 +0600

    add: create shop form

---
+++ src/app/(withCommonLayout)/create-shop/page.tsx
@@ -0,0 +1,11 @@
+import CreateShopForm from "@/components/modules/shop/create-shop/CreateShopForm";
+
+const CreateShopPage = () => {
+  return (
+    <div className="min-h-screen bg-cover bg-center flex items-center justify-center">
+      <CreateShopForm />
+    </div>
+  );
+};
+
+export default CreateShopPage;
